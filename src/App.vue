930c2174647da75ba41343e2dcfba8a49d5bd8e1
Pooja Lokesh <poojadeelok@gmail.com>
2020-08-23 11:28:04 +0530

    Music_app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
-
+    <header>
+      <h1>My music</h1>
+    </header>
+    <main>
+      <section class="player">
+        <h2 class="song-title">{{ current.title }} - <span>{{ current.artist }}</span></h2>
+        <div>
+          
+        </div>
+      </section>
+    </main>
   </div>
 </template>
 
@@ -8,16 +18,48 @@
 
 export default {
   name: 'App',
+  data () {
+    return {
+      current: {},
+      index: 0,
+      songs: [
+        {
+          title: 'Monster',
+          artist: 'DAQ',
+          src: require('./assets/battleclip_daq.mp3')
+        },
+        {
+          title: 'Hipe',
+          artist: 'ADIAO',
+          src: require('./assets/callout_adiao.mp3')
+        }
+      ],
+      player: new Audio()
+    }
+  },
+  created () {
+    this.current = this.songs(this.index);
+    this.player.src = this.current.src;
+    
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: sans-serif;
+}
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+  background-color: #212121;
+  color: #ffffff;
 }
 </style>
